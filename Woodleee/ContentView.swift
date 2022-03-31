edccefd9367cdb391249f1ede3dee6fc20eafb8d
BananaChiu <agimz0330@gmail.com>
2022-04-01 04:05:30 +0800

    Initial Commit

---
+++ Woodleee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Woodleee
+//
+//  Created by User23 on 2022/4/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Woodleee/WoodleeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoodleeeApp.swift
+//  Woodleee
+//
+//  Created by User23 on 2022/4/1.
+//
+
+import SwiftUI
+
+@main
+struct WoodleeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
